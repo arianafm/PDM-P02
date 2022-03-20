edff63c15f486745b44bdb4610daa9a0e2cb516e
Ariana Fernandez <arianafm@ciencias.unam.mx>
2022-03-20 17:13:26 -0600

    Agregando comentario_a

--- app/src/main/java/com/example/aplicacionpractica02/MainActivity.java
+++ app/src/main/java/com/example/aplicacionpractica02/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.aplicacionpractica02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * @author Ariana de Belen Fernandez Montenegro
+ * <a href="mailto:arianafm@ciencias.unam.mx" >arianafm@ciencias.unam.mx</a>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
