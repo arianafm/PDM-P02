242abe8fc8bc349901ea0a6c5c93009c8e06bb0f
miguel toledo <migueltr@Migueltr.local>
2022-03-20 17:13:18 -0600

    Agregando comentario_b.

--- app/src/main/java/com/example/aplicacionpractica02/MainActivity.java
+++ app/src/main/java/com/example/aplicacionpractica02/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.aplicacionpractica02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * @author Jose Miguerl Toledo Reyes -
+ * <a href="mailto:josmeigueltr@ciencias.unam.mx" >josemigueltr@ciencias.unam.mx</a>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
